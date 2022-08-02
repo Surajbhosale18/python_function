20e8f01a463a03ecac884776de5b825675d5cafb
Suraj Bhosale <87417300+Surajbhosale18@users.noreply.github.com>
2022-08-02 11:01:20 +0530

    main

---
+++ main.py
@@ -0,0 +1,11 @@
+# we are here creating function to convert seconds in minute
+
+def convert_seconds(seconds):
+    minute = seconds / 60
+    print(minute)
+
+convert_seconds(600)
+# first we create a function with the help of def key
+#then we pass the formula for minute as we know in 1 minute there are 60 seconds
+#and last print minute
+#and then call function
